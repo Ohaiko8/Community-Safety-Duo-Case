73636c5423cdc8faf5050a23545cc6bff4a4191a
Ohaiko8 <flinas2003@gmail.com>
2024-03-28 09:46:09 +0100

    XCode file created
    XCode file created

---
+++ Community Safety Duo App/Community Safety Duo App/Community_Safety_Duo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Community_Safety_Duo_AppApp.swift
+//  Community Safety Duo App
+//
+//  Created by Maksim Malko on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Community_Safety_Duo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
